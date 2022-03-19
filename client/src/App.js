39ae43df1f07768694256456b9ab267ed556fabf
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2022-03-19 12:46:54 +0100

    create route with request INSERT to database from form

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,22 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+
 function App() {
+    const [movieName, setMovieName] = useState("");
+    const [review, setReview] = useState("");
+
+    const submitReview = () => {
+        Axios.post("http://localhost:3001/api/insert", {
+            movieName: movieName,
+            movieReview: review,
+        }).then(() => {
+            console.log("succes insert");
+        });
+    }
+    
   return (
     <div className="App">
         <Container fluid="md">
@@ -17,15 +32,30 @@ function App() {
                     <Form>
                         <Form.Group className="mb-3" controlId="movieName">
                             <Form.Label>Movie name</Form.Label>
-                            <Form.Control name="movieName" type="text" placeholder="Movie name" />
+                            <Form.Control 
+                            name="movieName"
+                            onChange={(e) => {
+                                setMovieName(e.target.value);
+                            }}
+                            type="text"
+                            placeholder="Movie name" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="review">
                             <Form.Label>Movie review</Form.Label>
-                            <Form.Control type="review" placeholder="Review" />
+                            <Form.Control
+                            name="review"
+                            onChange={(e) => {
+                                setReview(e.target.value);
+                            }}
+                            type="text" 
+                            placeholder="Review" />
                         </Form.Group>
  
-                        <Button variant="primary" type="submit">
+                        <Button 
+                        variant="primary" 
+                        onClick={submitReview}
+                        type="submit">
                             S'inscrire
                         </Button>
                     </Form>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+const mysql = require("mysql");
+const cors = require('cors');
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "cruddatabase"
+});
 
-app.get("/", (req, res) => {
-    res.send("hello kirill");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/api/insert", (req, res) => {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+    //If error try in MySQL WorkBench ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?);";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(err)
+    });
 });
 app.listen(3001, () => {
-    console.log("listen to 3001")
+    console.log("listen on 3001")
 });
